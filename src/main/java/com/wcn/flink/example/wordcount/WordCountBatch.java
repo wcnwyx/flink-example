02ca42c2c2eacb7a1129bd266043249bc6415287
wucheng <1122wucheng@163.com>
2023-08-03 17:38:40 +0800

    init

---
+++ src/main/java/com/wcn/flink/example/wordcount/WordCountBatch.java
@@ -0,0 +1,31 @@
+package com.wcn.flink.example.wordcount;
+
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.AggregateOperator;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.operators.FlatMapOperator;
+import org.apache.flink.api.java.operators.UnsortedGrouping;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+public class WordCountBatch {
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment environment = ExecutionEnvironment.getExecutionEnvironment();
+
+        final DataSource<String> lineDataSource = environment.readTextFile("D://study//github//flink-example//src//input//words.txt");
+
+        final FlatMapOperator<String, Tuple2<String, Long>> flatMapOperator = lineDataSource.flatMap((String line, Collector<Tuple2<String, Long>> out) -> {
+            String[] words = line.split(" ");
+            for (String word : words) {
+                out.collect(Tuple2.of(word, 1L));
+            }
+        }).returns(Types.TUPLE(Types.STRING, Types.LONG));
+
+        final UnsortedGrouping<Tuple2<String, Long>> groupBy = flatMapOperator.groupBy(0);
+
+        final AggregateOperator<Tuple2<String, Long>> sum = groupBy.sum(1);
+
+        sum.print();
+    }
+}
